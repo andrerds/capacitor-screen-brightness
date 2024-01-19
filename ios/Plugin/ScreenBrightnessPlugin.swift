306a0e670598669409bc38b9aeae070e5bbaa2e1
Morgan Kikuchi-Conbere <morgan@tokentransit.com>
2024-01-19 15:01:36 -0800

    Keep the original and new brightness state in memory. Restore the original brightness when the app leaves the foreground and restore the new brightness when the app enters foreground.

--- ios/Plugin/ScreenBrightnessPlugin.swift
+++ ios/Plugin/ScreenBrightnessPlugin.swift
@@ -7,18 +7,48 @@ import Capacitor
  */
 @objc(ScreenBrightnessPlugin)
 public class ScreenBrightnessPlugin: CAPPlugin {
+    struct BrightnessState {
+        let original: CGFloat
+        let new: CGFloat
+    }
+    var state: BrightnessState? = .none
+
+    override public func load() {
+        NotificationCenter.default.addObserver(forName: UIApplication.willResignActiveNotification, object: nil, queue: .main) { [weak self] _ in
+            guard let self = self else { return }
+
+            if let state = self.state {
+                UIScreen.main.brightness = state.original
+            }
+        }
+        NotificationCenter.default.addObserver(forName: UIApplication.didBecomeActiveNotification, object: nil, queue: .main) { [weak self] _ in
+            guard let self = self else { return }
+
+            if let state = self.state {
+                self.state = BrightnessState(original: UIScreen.main.brightness, new: state.new)
+                UIScreen.main.brightness = state.new
+            }
+        }
+    }
 
     @objc func setBrightness(_ call: CAPPluginCall) {
         let brightness = call.getFloat("brightness", Float(UIScreen.main.brightness))
-        DispatchQueue.main.async {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else {
+                call.resolve()
+                return
+            }
+
+            let original = state?.original ?? UIScreen.main.brightness
+            state = BrightnessState(original: original, new: CGFloat(brightness))
             UIScreen.main.brightness = CGFloat(brightness)
             call.resolve()
         }
     }
-    
+
     @objc func getBrightness(_ call: CAPPluginCall) {
         call.resolve([
             "brightness": UIScreen.main.brightness
-        ])        
+        ])
     }
 }
